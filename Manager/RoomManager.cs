f4b9324abe7a9e7d785e9f9423b0a4113ed74880
Maxlamenace572 <maxence.barriergallet@gmail.com>
2026-05-16 03:47:48 +0200

    fix check biome master's keep

--- Manager/RoomManager.cs
+++ Manager/RoomManager.cs
@@ -92,7 +92,7 @@ namespace DeadCellsArchipelago {
                 }
                 PrepareBiomeCheck(ldat.id.ToString(), " Enter", ldat.id.ToString());
             }
-            if(new[] {"Ossuary", "QueenArena", "Bank", "DeathArena"}.Any(ldat.id.ToString().Contains))
+            if(new[] {"Ossuary", "QueenArena", "Bank", "DeathArena", "DookuArena"}.Any(ldat.id.ToString().Contains))
             {
                 ResetFrontPokebomb();
                 PrepareBiomeCheck(ldat.id.ToString(), " Enter", ldat.id.ToString());
